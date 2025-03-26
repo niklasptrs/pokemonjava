7adff19e3f862b8312b5ac0ee10e3017ea212a70
niklasptrs <niklas.p.2007@gmail.com>
2025-03-26 22:34:15 +0100

    Implemented introduction to the game Exchanged Golbit with Kleinstein

---
+++ Kleinstein.java
@@ -0,0 +1,48 @@
+
+public class Kleinstein extends Pokemon
+{
+    
+    
+    public Kleinstein()
+    {
+        typ1 = "Gestein";
+        typ2 = "Boden";
+        level = 1;
+        name = "Kleinstein";
+        leben = 40;
+        angriffstaerke = 80;
+        entwicklung1 = 25;
+        entname1 = "Georok";
+        lebenent1 = 55;
+        angent1 = 95;
+        
+        
+        //Angriffe
+        ang1 = "Tackle";
+        dang1 = 40;
+        angu1 = 1;
+        uang1 = true;
+        
+        ang2 = "Steinwurf";
+        dang2 = 50;
+        angu2 = 16;
+        uang2 = false;
+        
+        ang3 = "Finale";
+        dang3 = 130;
+        angu3 = 21;
+        uang3 = false;
+        
+        ang4 = "Erdbeben";
+        dang4 = 100;
+        angu4 = 31;
+        uang4 = false;
+        
+        ang5 = "Explosion";
+        dang5 = 170;
+        angu5 = 36;
+        uang5 = false;
+    }
+
+    
+}
--- Trainer.java
+++ Trainer.java
@@ -2,22 +2,75 @@ import java.util.Scanner;
 public class Trainer
 {
     Glumanda glumanda1;
-    Golbit golbit1;
+    Kleinstein kleinstein1;
     Taubsi taubsi1;
-    
+
     boolean hatGlumanda;
     boolean hatGolbit;
     boolean hatTaubsi;
-    
+
     int pokemon;
-    
+
+    boolean introgesehen;
+    int abfrage;
+
     public Trainer()
     {
         glumanda1 = new Glumanda();
         taubsi1 = new Taubsi();
-        golbit1 = new Golbit();
-        
-       
+        kleinstein1 = new Kleinstein();
+
+        System.out.println("/====================================================================\\");
+        System.out.println("||########---#######--##----##-########-##-----##--#######--##----##||");
+        System.out.println("||##-----##-##-----##-##---##--##-------###---###-##-----##-###---##||");
+        System.out.println("||##-----##-##-----##-##--##---##-------####-####-##-----##-####--##||");
+        System.out.println("||########--##-----##-#####----######---##-###-##-##-----##-##-##-##||");
+        System.out.println("||##--------##-----##-##--##---##-------##-----##-##-----##-##--####||");
+        System.out.println("||##--------##-----##-##---##--##-------##-----##-##-----##-##---###||");
+        System.out.println("||##---------#######--##----##-########-##-----##--#######--##----##||");
+        System.out.println("\\====================================================================/");
+
+        //Begrüßung
+
+        schreiben("Noch kannst du das Tutorial überspringen (Entwicklungsversion) mit 2");
+        Scanner scanner = new Scanner(System.in);
+        int abfrage = Integer.parseInt(scanner.nextLine());
+        if(abfrage == 2){
+            introgesehen = true;
+        }
+        else if( introgesehen == false){
+            schreiben("Willkommen in der Welt der Pokémon!");
+            System.out.println("");
+
+            schreiben("Du bist ein angehender Pokémon-Trainer.");
+            System.out.println("");
+
+            schreiben("Dein Abenteuer beginnt heute.");
+            System.out.println("");
+
+            schreiben("In einer Welt voller geheimnisvoller Kreaturen und spannender Herausforderungen.");
+            System.out.println("");
+
+            schreiben("Es liegt an dir, ein starkes Team aufzubauen.");
+            System.out.println("");
+
+            schreiben("Knüpfe Freundschaften und werde der beste Trainer.");
+            System.out.println("");
+
+            schreiben("Wähle dein erstes Pokémon weise.");
+            System.out.println("");
+
+            schreiben("Deine Reise wird voller Überraschungen und spannender Kämpfe sein.");
+            System.out.println("");
+
+            schreiben("Bist du bereit, dich der Herausforderung zu stellen?");
+            System.out.println("");
+
+            schreiben("Dein eigener Legendenpfad wartet auf dich!");
+            System.out.println("");
+
+        }
+
         /*
         System.out.println("/==============================================\\");
         System.out.println("||  ____       _                              ||");
@@ -27,20 +80,20 @@ public class Trainer
         System.out.println("|| |_|   \\___/|_|\\_\\___|_| |_| |_ \\___/|_| |_|||");
         System.out.println("||                                            ||");
         System.out.println("\\==============================================/");
-    */
+         */
     }
 
     public void pokemonEntwickeln(){
         System.out.println("Welches Pokemon möchtest du entwickeln?");
         System.out.println("Pokemon 1 = "+glumanda1.name);
         System.out.println("Pokemon 2 = "+taubsi1.name);
-        System.out.println("Pokemon 3 = "+golbit1.name);
-        
+        System.out.println("Pokemon 3 = "+kleinstein1.name);
+
         Scanner scanner = new Scanner(System.in);
         int pokemon = Integer.parseInt(scanner.nextLine());
-        
+
         if (pokemon >= 4){
-           System.out.println("Ungültiger Wert!"); 
+            System.out.println("Ungültiger Wert!"); 
         }
         else if (pokemon == 1){
             glumanda1.entwickeln();
@@ -49,20 +102,21 @@ public class Trainer
             taubsi1.entwickeln();
         }
         else if (pokemon == 3){
-            golbit1.entwickeln();
+            kleinstein1.entwickeln();
         }
     }
+
     public void pokemonleveln(){
         System.out.println("Welches Pokemon möchtest du leveln?");
         System.out.println("Pokemon 1 = "+glumanda1.name);
         System.out.println("Pokemon 2 = "+taubsi1.name);
-        System.out.println("Pokemon 3 = "+golbit1.name);
-        
+        System.out.println("Pokemon 3 = "+kleinstein1.name);
+
         Scanner scanner = new Scanner(System.in);
         int pokemon = Integer.parseInt(scanner.nextLine());
-        
+
         if (pokemon >= 4){
-           System.out.println("Ungültiger Wert!"); 
+            System.out.println("Ungültiger Wert!"); 
         }
         else if (pokemon == 1){
             glumanda1.lvl100();
@@ -73,8 +127,23 @@ public class Trainer
             System.out.println("Erfolgreich gelevelt.");
         }
         else if (pokemon == 3){
-            golbit1.lvl100();
+            kleinstein1.lvl100();
             System.out.println("Erfolgreich gelevelt.");
+        }
+
+    }
+
+    public void schreiben(String pText){
+        String text = pText;
+
+        for (char buchstabe : text.toCharArray()) {
+            System.out.print(buchstabe); // Gibt Buchstaben einzeln aus
+            try {
+                Thread.sleep(30); // Pause von 100 Millisekunden
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
